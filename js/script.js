1f770403c5d84e1b7d9cdab19293b9e7cafd0298
Łukasz Kuczek <lkuczek@wp.pl>
2017-10-12 02:18:55 +0200

    Ready v2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,9 @@
 (function($) {
-    $(document).ready(function() {
-        var header = $("header"),
-            footer = $(".footer nav"),
-            content = $(".content .container .row"),
-            hOuterHeight = header.outerHeight(true),
-            fOuterHeight = footer.outerHeight(true),
-            contentHeight = ($(window).height() - (hOuterHeight + fOuterHeight));
-
-        content.outerHeight(contentHeight);
-        console.log("Wysokość header`a: " + hOuterHeight);
-        console.log("Wysokość footer`a: " + fOuterHeight);
-        console.log("Wysokość okna: " + $(window).height());
-        console.log("Wysokość contentu: " + content.outerHeight(true));
+    var  div = $(".show-content");
+    div.on("showContent", function() {
+        $(this).addClass("visible");
+    });
+    $(window).on("load", function() {
+        div.trigger("showContent");
     });
 })(jQuery);
